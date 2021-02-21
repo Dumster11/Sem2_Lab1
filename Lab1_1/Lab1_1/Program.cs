536d786c11b8bfc516bf2d26235a1cd79d4fa1d0
Dumster11 <71654055+Dumster11@users.noreply.github.com>
2021-02-21 12:03:45 +0200

    sd

---
+++ Lab1_1/Lab1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1_1
+{
+    public delegate int Sr_ar(int x, int y, int z);
+    class Program
+    {
+        private static int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static void Main(string[] args)
+        {
+            Sr_ar operation = Add;
+            operation += delegate (int x, int y, int z) { return (x + y + z) / 3; };
+            Console.WriteLine(operation(6, 1, 7));
+            Console.ReadKey();
+
+        }
+    }
+}
