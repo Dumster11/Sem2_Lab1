184f729d788b7f92df73a11589910e553b8f5ff7
Dumster11 <71654055+Dumster11@users.noreply.github.com>
2021-02-21 12:25:37 +0200

    sd

---
+++ Lab1_2/Lab1_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    public delegate int Operation(int x, int y);
+    class Program
+    {
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        private static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        private static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        private static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main(string[] args)
+        {
+            Operation op = Add;
+            op += (x, y) => { return x + y; };
+            Console.WriteLine("Сумма (x+y)="+op(3, 6));
+            Operation op1 = Sub;
+            op += (x, y) => { return x - y; };
+            Console.WriteLine("Вычитание (x-y)="+op(7, 1));
+            Operation op2 = Mult;
+            op += (x, y) => { return x * y; };
+            Console.WriteLine("Умножение (x*y)="+op(1, 9));
+            Operation op3 = Div;
+            op += (x, y) => { return x / y; };
+            Console.WriteLine("Деление (x/y)="+op(2, 4));
+            Console.ReadLine();
+        }
+    }
+} 
